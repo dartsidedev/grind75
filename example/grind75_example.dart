cc8f0557d6f09d48662619ab78da7445f44a02b0
Vince Varga <vince.varga@dartside.dev>
2022-07-26 11:12:44 +0200

    Create package

---
+++ example/grind75_example.dart
@@ -0,0 +1,6 @@
+import 'package:grind75/grind75.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/grind75.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library grind75;
+
+export 'src/grind75_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
