fbe642ff78857f4937b049b79dd7946b82027ca1
Vince Varga <vince.varga@dartside.dev>
2022-07-27 16:15:45 +0200

    Palindrome Number: reverted

--- test/palindrome_number_reverted_test.dart
+++ test/palindrome_number_reverted_test.dart
@@ -2,22 +2,17 @@ import 'package:test/test.dart';
 
 bool palindromeNumber(int value) {
   if (value.isNegative) return false;
+  if (value == 0) return true;
+  if (value % 10 == 0) return false;
 
-  final list = _convert(value);
-  for (int i = 0; i < (list.length ~/ 2); i++) {
-    if (list[i] != list[list.length - 1 - i]) return false;
+  var reverted = 0, iter = value;
+  while (iter > 0) {
+    reverted = reverted * 10;
+    reverted += iter % 10;
+    iter = iter ~/ 10;
   }
 
-  return true;
-}
-
-List<int> _convert(int value) {
-  final list = <int>[];
-  while (value > 0) {
-    list.add(value % 10);
-    value = value ~/ 10;
-  }
-  return list;
+  return reverted == value;
 }
 
 void main() {
